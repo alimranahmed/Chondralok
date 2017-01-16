8a71bc44479378c659026d20f1ce258020bc51df
Al- Imran Ahmed <al.imran.cse@gmail.com>
2017-01-16 23:46:32 +0600

    Key press listener working..

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import json
+import tkinter as tk
 
 with open('draft_map.json') as letter_map_file:
     banglaToEnglish = json.load(letter_map_file)
@@ -6,3 +7,13 @@ with open('draft_map.json') as letter_map_file:
 englishToBangala = {value: key for key, value in banglaToEnglish.items()}
 
 print(englishToBangala['k']+englishToBangala['e'])
+
+def onKeyPress(event):
+    text.insert('end', '%s' % englishToBangala[event.char])
+
+root = tk.Tk()
+root.geometry('300x200')
+text = tk.Text(root, background='white', foreground='black', font=('Comic Sans MS', 12))
+text.pack()
+root.bind('<KeyPress>', onKeyPress)
+root.mainloop()
