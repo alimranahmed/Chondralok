b6d90a5a51e36958785ed56d3a78aedfdb39c7ab
Al- Imran Ahmed <al.imran.cse@gmail.com>
2017-01-24 01:28:41 +0600

    Map file name changed

--- Eng2BanMap.py
+++ Eng2BanMap.py
@@ -4,7 +4,7 @@ import json
 class Eng2BanMap:
 
     def __init__(self):
-        with open('draft_map.json') as letter_map_file:
+        with open('bengali_to_english.json') as letter_map_file:
             bengali_to_english = json.load(letter_map_file)
 
         self.english_to_bengali = {value: key for key, value in bengali_to_english.items()}
