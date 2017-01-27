20733500d060cf17fd204636fba5db3d60881a22
Al- Imran Ahmed <al.imran.cse@gmail.com>
2017-01-27 23:27:15 +0600

    Turtle shape has been created

--- src/TurtleWindow.py
+++ src/TurtleWindow.py
@@ -1,12 +1,32 @@
 import turtle
 
-def draw_triangle():
-    window = turtle.Screen()
-    window.bgcolor("red")
 
-    painter = turtle.Turtle()
-    painter.forward(100)
+def draw_square(turtle_painter):
+    turtle_painter.color("green")
+
+    for i in range(1, 5):
+        turtle_painter.forward(100)
+        turtle_painter.right(90)
+
+
+def draw_art():
+    window = turtle.Screen()
+    window.bgcolor("white")
+
+    turtle_painter = turtle.Turtle()
+    turtle_painter.shape("turtle")
+
+    for i in range(1, 37):
+        draw_square(turtle_painter)
+        turtle_painter.right(10)
+
+    draw_circle(turtle_painter)
 
     window.exitonclick()
 
-draw_triangle()
+
+def draw_circle(turtle_painter):
+    turtle_painter.color("blue")
+    turtle_painter.circle(100)
+
+draw_art()
