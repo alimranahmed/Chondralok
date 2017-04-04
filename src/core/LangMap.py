a2246e8b8fb41f71915dd1f21307e598d8ab0a42
Al- Imran Ahmed <al.imran.cse@gmail.com>
2017-04-05 00:34:41 +0600

    Newly structured json is being parsed

--- src/core/LangMap.py
+++ src/core/LangMap.py
@@ -1,11 +1,27 @@
 import json
+import datetime
 
 
 class LangMap:
 
     def __init__(self):
-        print("JSON parsing...")
-        with open('bengali_to_english.json') as letter_map_file:
-            self.english_to_bengali = json.load(letter_map_file)
+        LangMap.log(self, "__init__", "Initiated...")
+
+        self.english_to_bengali = self.load_json('../assets/jsons/english_to_bangla_map.json')
+
+        LangMap.log(self, "__init__", "Json parsed!")
         print(self.english_to_bengali)
-        print('JSON parsed')
+
+    def load_json(self, json_path):
+        LangMap.log(self, "load_json", "called...")
+        with open(json_path) as letter_map_file:
+            return json.load(letter_map_file)
+
+    @staticmethod
+    def log(class_name, method_name, message):
+        log_msg = "["+str(datetime.datetime.now())+"]"
+        log_msg += "[" + class_name.__class__.__name__ + "][" + method_name + "] "
+        log_msg += message
+        print(log_msg)
+
+langMap = LangMap()
