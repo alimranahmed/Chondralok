77102fd9f650803b48fbc854605425de38c58f90
Al- Imran Ahmed <al.imran.cse@gmail.com>
2017-04-06 21:56:52 +0600

    Non dependent code move to Utility class

--- src/core/LangMap.py
+++ src/core/LangMap.py
@@ -1,20 +1,10 @@
-import json
 from src.core.Utility import Utility
 
 
 class LangMap:
 
     def __init__(self):
-        Utility.log(self, "__init__", "Initiated...")
-
-        self.english_to_bengali = self.load_json('../assets/jsons/english_to_bangla_map.json')
-
-        Utility.log(self, "__init__", "Json parsed!")
+        self.english_to_bengali = Utility.load_json('../assets/jsons/english_to_bangla_map.json')
         print(self.english_to_bengali)
-
-    def load_json(self, json_path):
-        Utility.log(self, "load_json", "called...")
-        with open(json_path) as letter_map_file:
-            return json.load(letter_map_file)
 
 langMap = LangMap()
--- src/core/Utility.py
+++ src/core/Utility.py
@@ -1,4 +1,5 @@
 import datetime
+import json
 
 
 class Utility:
@@ -8,3 +9,8 @@ class Utility:
         log_msg += "[" + class_name.__class__.__name__ + "][" + method_name + "] "
         log_msg += message
         print(log_msg)
+
+    @staticmethod
+    def load_json(json_path):
+        with open(json_path) as letter_map_file:
+            return json.load(letter_map_file)
