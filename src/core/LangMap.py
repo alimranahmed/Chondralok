198358edae2fce85554db114e9570406fbedb685
Al- Imran Ahmed <al.imran.cse@gmail.com>
2017-04-06 21:59:37 +0600

    Jsons folder renamed to json

--- src/core/LangMap.py
+++ src/core/LangMap.py
@@ -4,7 +4,8 @@ from src.core.Utility import Utility
 class LangMap:
 
     def __init__(self):
-        self.english_to_bengali = Utility.load_json('../assets/jsons/english_to_bangla_map.json')
+        json_path = '../assets/json/english_to_bangla_map.json'
+        self.english_to_bengali = Utility.load_json(json_path)
         print(self.english_to_bengali)
 
 langMap = LangMap()
