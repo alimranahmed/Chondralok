8b32484f32fb752524bf7649cf4b489fdbe1ef86
Al- Imran Ahmed <al.imran.cse@gmail.com>
2017-04-07 12:28:53 +0600

    Previous engtobanMap class move to core and renamed as Engine

--- src/core/Engine.py
+++ src/core/Engine.py
@@ -3,7 +3,7 @@ from src.core.LangMap import langMap
 
 class Eng2BanMap:
     def __init__(self):
-        self.lang_map = langMap()
+        self.lang_map = langMap
 
     def get_bengali_character(self, english_character, last_char=' '):
         if english_character not in self.lang_map.eng_to_ban_map:
