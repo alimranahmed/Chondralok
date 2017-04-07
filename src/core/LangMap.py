a30e9749df91f2bf8d34d6a8ee74ffa163df5d22
Al- Imran Ahmed <al.imran.cse@gmail.com>
2017-04-07 12:08:21 +0600

    English to bangla map list processed to map

--- src/core/LangMap.py
+++ src/core/LangMap.py
@@ -4,8 +4,19 @@ from src.core.Utility import Utility
 class LangMap:
 
     def __init__(self):
-        json_path = '../assets/json/english_to_bangla_map.json'
-        self.english_to_bengali = Utility.load_json(json_path)
-        print(self.english_to_bengali)
+        letter_map_list = self.load_map()
+        self.eng_to_ban_map = self.make_dict(letter_map_list)
+        print(self.eng_to_ban_map)
 
+    @staticmethod
+    def make_dict(letter_map_list):
+        eng_to_ban_map = {}
+        for letter in letter_map_list:
+            eng_to_ban_map[letter['search_for']] = letter
+        return eng_to_ban_map
+
+    @staticmethod
+    def load_map():
+        json_path = '../assets/json/english_to_bangla_map.json'
+        return Utility.load_json(json_path)
 langMap = LangMap()
