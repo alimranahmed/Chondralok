5d129d3b1454865997af2a97c67ade2f8f4bb4ab
Al- Imran Ahmed <al.imran.cse@gmail.com>
2017-04-10 10:18:25 +0600

    #1: Convertor Engine is converting combine english latter into a bangla latter

--- src/core/Engine.py
+++ src/core/Engine.py
@@ -3,17 +3,32 @@ from src.core.LangMap import langMap
 
 class Eng2BanMap:
     def __init__(self):
-        self.lang_map = langMap
+        self.eng_to_ban_map = langMap.eng_to_ban_map
 
-    def get_bengali_character(self, english_character, last_char=' '):
-        if english_character not in self.lang_map.eng_to_ban_map:
+    def get_bengali_character(self, english_character, last_eng_char=' '):
+        print(last_eng_char+english_character)
+        # letters with more than one character
+        # TODO create separate method
+        if last_eng_char+english_character in self.eng_to_ban_map:
+            print('is in map')
+            if self.eng_to_ban_map[last_eng_char+english_character]['type'] == 'vowel':
+                if last_eng_char is ' ':
+                    print('last is space')
+                    return self.eng_to_ban_map[last_eng_char+english_character]['replace']
+                else:
+                    print('last is not space')
+                    return self.eng_to_ban_map[last_eng_char+english_character]['short_form']
+            else:
+                return self.eng_to_ban_map[last_eng_char+english_character]['replace']
+        # TODO separate this method also
+        if english_character not in self.eng_to_ban_map:
             return english_character
-        if self.lang_map.eng_to_ban_map[english_character]['type'] == 'vowel':
-            if last_char is ' ':
+        if self.eng_to_ban_map[english_character]['type'] == 'vowel':
+            if last_eng_char is ' ':
                 print('last is space')
-                return self.lang_map.eng_to_ban_map[english_character]['replace']
+                return self.eng_to_ban_map[english_character]['replace']
             else:
                 print('last is not space')
-                return self.lang_map.eng_to_ban_map[english_character]['short_form']
+                return self.eng_to_ban_map[english_character]['short_form']
         else:
-            return self.lang_map.eng_to_ban_map[english_character]['replace']
+            return self.eng_to_ban_map[english_character]['replace']
