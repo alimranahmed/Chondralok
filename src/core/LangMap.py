226bcadc5b26c99de4d626326f5ce9012d096769
Al- Imran Ahmed <al.imran.cse@gmail.com>
2017-04-10 10:18:49 +0600

    File path issue temporarily fixed

--- src/core/LangMap.py
+++ src/core/LangMap.py
@@ -4,7 +4,7 @@ from src.core.Utility import Utility
 class LangMap:
 
     def __init__(self):
-        letter_map_list = Utility.load_json('../assets/json/english_to_bangla_map.json')
+        letter_map_list = Utility.load_json('/var/www/html/Chondralok/src/assets/json/english_to_bangla_map.json')
         self.eng_to_ban_map = self.make_dict(letter_map_list)
         print(self.eng_to_ban_map)
 
