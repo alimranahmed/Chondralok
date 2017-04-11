4a68c2879a41fb684335dec5b7eb39dd93441f30
Al- Imran Ahmed <al.imran.cse@gmail.com>
2017-04-11 10:06:07 +0600

    #1: Combine english char are now reusing the same method for single english char

--- src/core/Engine.py
+++ src/core/Engine.py
@@ -5,30 +5,26 @@ class Eng2BanMap:
     def __init__(self):
         self.eng_to_ban_map = langMap.eng_to_ban_map
 
-    def get_bengali_character(self, english_character, last_eng_char=' '):
-        print(last_eng_char+english_character)
+    def get_bengali_character(self, eng_char, last_eng_char=' '):
+        print(last_eng_char+eng_char)
+
         # letters with more than one character
-        # TODO create separate method
-        if last_eng_char+english_character in self.eng_to_ban_map:
+        if last_eng_char+eng_char in self.eng_to_ban_map:
             print('is in map')
-            if self.eng_to_ban_map[last_eng_char+english_character]['type'] == 'vowel':
-                if last_eng_char is ' ':
-                    print('last is space')
-                    return self.eng_to_ban_map[last_eng_char+english_character]['replace']
-                else:
-                    print('last is not space')
-                    return self.eng_to_ban_map[last_eng_char+english_character]['short_form']
-            else:
-                return self.eng_to_ban_map[last_eng_char+english_character]['replace']
-        # TODO separate this method also
-        if english_character not in self.eng_to_ban_map:
-            return english_character
-        if self.eng_to_ban_map[english_character]['type'] == 'vowel':
+            return self.get_char_to_replace(last_eng_char+eng_char, last_eng_char)
+
+        return self.get_char_to_replace(eng_char, last_eng_char)
+
+    def get_char_to_replace(self, eng_chars, last_eng_char):
+        if eng_chars not in self.eng_to_ban_map:
+            return eng_chars
+
+        if self.eng_to_ban_map[eng_chars]['type'] == 'vowel':
             if last_eng_char is ' ':
                 print('last is space')
-                return self.eng_to_ban_map[english_character]['replace']
+                return self.eng_to_ban_map[eng_chars]['replace']
             else:
                 print('last is not space')
-                return self.eng_to_ban_map[english_character]['short_form']
+                return self.eng_to_ban_map[eng_chars]['short_form']
         else:
-            return self.eng_to_ban_map[english_character]['replace']
+            return self.eng_to_ban_map[eng_chars]['replace']
