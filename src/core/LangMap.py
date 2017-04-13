10711fa98c26186e14fb18a8284f1bdaee64fdab
Al- Imran Ahmed <al.imran.cse@gmail.com>
2017-04-13 22:57:32 +0600

    Map will work even if on bangla character has more than one english character

--- src/core/LangMap.py
+++ src/core/LangMap.py
@@ -12,7 +12,11 @@ class LangMap:
     def make_dict(letter_map_list):
         eng_to_ban_map = {}
         for letter in letter_map_list:
-            eng_to_ban_map[letter['search_for']] = letter
+            if type(letter['search_for']) is list:
+                for search_for in letter['search_for']:
+                    eng_to_ban_map[search_for] = letter
+            else:
+                eng_to_ban_map[letter['search_for']] = letter
         return eng_to_ban_map
 
 langMap = LangMap()
